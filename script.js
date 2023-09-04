a75252f4d21245a129823faa3fd86b2f35852b83
swamfire <usern5949@gmail.com>
2023-09-04 12:36:52 +0530

    added high and low value comparision

--- script.js
+++ script.js
@@ -16,12 +16,42 @@ console.log(document.querySelector(".guess").value)
 // document.querySelector(".check").addEventListener("click", function(){
 //     console.log(document.querySelector(".guess").value)
 // })
+let score = 20
+
+const secretNumber=Math.trunc(Math.random()*20) + 1
+document.querySelector(".number").textContent = secretNumber
 
 document.querySelector(".check").addEventListener("click", ()=>{
     const guess = Number(document.querySelector(".guess").value)
     // console.log(guess, typeof guess)
     if(!guess){
         document.querySelector(".message").textContent = "No Number !!!"
+    }
+    else if(guess === secretNumber){
+        document.querySelector('.message').textContent="correct number !!";
+
+    }
+    else if(guess>secretNumber){
+        if(score > 1){
+        document.querySelector('.message').textContent="Too High !!";
+        score--
+        document.querySelector(".score").textContent = score
+        }
+        else{
+            document.querySelector(".score").textContent = 0
+            document.querySelector('.message').textContent="You lost the game!!!";
+        }
+    }
+    else if(guess<secretNumber){
+        if(score>1){
+            document.querySelector('.message').textContent="Too Low !!";
+            score--
+            document.querySelector(".score").textContent = score
+        }
+        else{
+            document.querySelector(".score").textContent = 0
+            document.querySelector('.message').textContent="You lost the game!!!";
+        }
     }
     // document.querySelector(".message").textContent = "Correct Number !"
 })
