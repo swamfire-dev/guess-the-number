e20be1bf0d129d1d7d8e9b5230a32310a0c58c1e
swamfire <usern5949@gmail.com>
2023-09-04 12:19:15 +0530

    added DOM manipulation

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 //DOM -> Document Object Models
 console.log(document.querySelector(".message").textContent)
 document.querySelector(".message").textContent = "Correct Number !"
@@ -10,3 +10,18 @@ document.querySelector(".score").textContent = 10
 console.log(document.querySelector(".guess").value)
 document.querySelector(".guess").value = 23
 console.log(document.querySelector(".guess").value)
+*/
+
+// console.log(document.querySelector(".check"))
+// document.querySelector(".check").addEventListener("click", function(){
+//     console.log(document.querySelector(".guess").value)
+// })
+
+document.querySelector(".check").addEventListener("click", ()=>{
+    const guess = Number(document.querySelector(".guess").value)
+    // console.log(guess, typeof guess)
+    if(!guess){
+        document.querySelector(".message").textContent = "No Number !!!"
+    }
+    // document.querySelector(".message").textContent = "Correct Number !"
+})
