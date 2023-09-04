acbe72d33ab6a6c385cc13834cad2791582c8153
swamfire <usern5949@gmail.com>
2023-09-04 12:07:23 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+//DOM -> Document Object Models
+console.log(document.querySelector(".message").textContent)
+document.querySelector(".message").textContent = "Correct Number !"
+// console.log(document.querySelector(".message").textContent)
+document.querySelector(".number").textContent = 13
+document.querySelector(".score").textContent = 10
+
+console.log(document.querySelector(".guess").value)
+document.querySelector(".guess").value = 23
+console.log(document.querySelector(".guess").value)
