e0ab6b1066ffdbd6d414e5eab4984a619d95e849
swamfire <usern5949@gmail.com>
2023-09-05 12:59:21 +0530

    removed unnecessary comments

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
 'use strict';
-/*
-//DOM -> Document Object Models
-console.log(document.querySelector(".message").textContent)
-document.querySelector(".message").textContent = "Correct Number !"
-// console.log(document.querySelector(".message").textContent)
-document.querySelector(".number").textContent = 13
-document.querySelector(".score").textContent = 10
-
-console.log(document.querySelector(".guess").value)
-document.querySelector(".guess").value = 23
-console.log(document.querySelector(".guess").value)
-*/
-
-// console.log(document.querySelector(".check"))
-// document.querySelector(".check").addEventListener("click", function(){
-//     console.log(document.querySelector(".guess").value)
-// })
 let score = 20
 let secretNumber=Math.trunc(Math.random()*20) + 1
 let highscore = 0
@@ -25,10 +8,8 @@ const displayMessage = function(message){
 }
 
 document.querySelector(".again").addEventListener("click", ()=>{
-    // document.querySelector('.message').textContent="Again clicked !!";
     score=20
     secretNumber = Math.trunc(Math.random()*20) + 1
-    // document.querySelector('.message').textContent= "Start Guessing ..."
     displayMessage("Start Guessing ...")
     document.querySelector(".score").textContent = score
     document.querySelector(".number").textContent = "?"
@@ -40,13 +21,10 @@ document.querySelector(".again").addEventListener("click", ()=>{
 
 document.querySelector(".check").addEventListener("click", ()=>{
     const guess = Number(document.querySelector(".guess").value)
-    // console.log(guess, typeof guess)
     if(!guess){
-        // document.querySelector(".message").textContent = "No Number !!!"
         displayMessage("No Number !!!")
     }
     else if(guess === secretNumber){
-        // document.querySelector('.message').textContent="correct number !!";
         displayMessage("Correct Number !!")
         document.querySelector("body").style.backgroundColor = "#60b347"
         document.querySelector(".number").style.width = "30rem"
@@ -63,30 +41,6 @@ document.querySelector(".check").addEventListener("click", ()=>{
             }
         else{
             document.querySelector(".score").textContent = 0
-            // document.querySelector('.message').textContent="You lost the game!!!";
             displayMessage("You lost the game!!!")
         }
-    // else if(guess>secretNumber){
-    //     if(score > 1){
-    //     document.querySelector('.message').textContent="Too High !!";
-    //     score--
-    //     document.querySelector(".score").textContent = score
-    //     }
-    //     else{
-    //         document.querySelector(".score").textContent = 0
-    //         document.querySelector('.message').textContent="You lost the game!!!";
-    //     }
-    // }
-    // else if(guess<secretNumber){
-    //     if(score > 1){
-    //         document.querySelector('.message').textContent="Too Low !!";
-    //         score--
-    //         document.querySelector(".score").textContent = score
-    //     }
-    //     else{
-    //         document.querySelector(".score").textContent = 0
-    //         document.querySelector('.message').textContent="You lost the game!!!";
-    //     }
-    // }
-    // document.querySelector(".message").textContent = "Correct Number !"
 })
